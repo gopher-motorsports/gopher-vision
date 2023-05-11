474030cefa55a79dfa81169521dc77d1058f7c1a
olale013 <95550079+olale013@users.noreply.github.com>
2023-05-10 19:31:21 -0500

    implement to-csv

--- to-csv.py
+++ to-csv.py
@@ -1,9 +1,24 @@
 import sys
 from pathlib import Path
+import csv
 import go4v
+
+if len(sys.argv) < 3:
+    sys.exit('ERROR: expected "python to-csv.py input.gdat output.csv"')
 
 ipath = Path(sys.argv[1])
 opath = Path(sys.argv[2])
 
-data = ipath.read_bytes()
-print(f'Read {len(data)} bytes from {ipath.name}.')
+(filename, extension, data) = ipath.read_bytes().partition(b'.gdat:')
+print(f'Read {len(data)} bytes from {ipath.name}')
+print(f'METADATA: "{(filename + extension).decode()}"')
+
+packets = [go4v.parse(go4v.unescape(p)) for p in go4v.split(data)]
+errors = sum(not p['valid'] for p in packets)
+
+print(f'ERROR RATE: {round(errors / len(packets), 5)}%')
+
+with open(opath, 'w', newline='') as ofile:
+    writer = csv.DictWriter(ofile, fieldnames=['hex', 'timestamp', 'id', 'data', 'valid'])
+    writer.writeheader()
+    writer.writerows(packets)
