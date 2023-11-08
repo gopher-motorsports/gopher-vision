52636c7a9f6e551b66d2ac172e23b6cee5621d27
Jonathan Olaleye <69396515+j0la@users.noreply.github.com>
2023-11-08 03:22:29 -0600

    cleanup

--- gdat.py
+++ gdat.py
@@ -76,14 +76,13 @@ def decode_packets(bytes, channels, parameters):
 # value = encoded_value * 10^-shift * scalar / divisor
 # encoded_value = value / 10^-shift / scalar * divisor
 # to make the most of a s16: abs_max = (2^15 - 1) * 10^-shift * scalar / divisor
-def get_scalars(id, v_min, v_max):
+def get_scalars(v_min, v_max):
     abs_max = max(abs(v_min), abs(v_max))
 
     exp = 0
     scale = abs_max
     scf = 0
     shift = 0
-    offset = 0
     done = False
 
     if v_max - v_min <= 0.001:
@@ -106,40 +105,40 @@ def get_scalars(id, v_min, v_max):
         exp += 1
 
     if not done:
-        print(f'failed to find scalars for ch={id} min={v_min} max={v_max}')
+        raise Exception('encoding failed')
     
     scalar, divisor = Fraction(scf).limit_denominator(2047).as_integer_ratio()
     if scalar > 2047:
-        scalar = 0
-        divisor = 0
-        shift = 0
+        raise Exception('encoding failed')
     
-    return (shift, scalar, divisor, scf)
+    return (shift, scalar, divisor)
 
 def parse(bytes, parameters):
     channels = {
         id: {
+            # raw data
             'id': id,
             'name': param['name'],
             'unit': param['unit'],
-            'sample_count': 0,
+            'n_points': 0,              # num raw datapoints
+            't_min': 0,                 # min datapoint timestamp
+            't_max': 0,                 # max datapoint timestamp
+            'v_min': 0,                 # min datapoint value
+            'v_max': 0,                 # max datapoint value
+            'points': [],
+            # interpolated data
             'delta_ms': 0,
             'frequency_hz': 0,
-            't_min': 0,
-            't_max': 0,
-            'v_min': 0,
-            'v_max': 0,
-            # scalars to encode in s16
+            'sample_count': 0,
+            't_int': [],                # evenly spaced timestamps
+            'v_int': [],                # interpolated values
+            # s32 encoded data (on t_int time axis)
+            # encoded_value = value / 10^-shift / scalar * divisor
+            'v_enc': [],
             'shift': 0,
-            'scalar': 1,
-            'divisor': 1,
+            'scalar': 0,
+            'divisor': 0,
             'offset': 0,
-            # data
-            'points': [],
-            'num_points': 0,
-            't_int': None, # interpolated timestamps
-            'v_int': None, # interpolated values
-            'v_enc': None, # encoded values
         }
         for (id, param) in parameters.items()
     }
@@ -158,8 +157,8 @@ def parse(bytes, parameters):
     print('sorting data... ', end='', flush=True)
     start = time.time()
     for ch in channels.values():
+        ch['n_points'] = len(ch['points'])
         # sort points by timestamp
-        ch['num_points'] = len(ch['points'])
         ch['points'].sort(key=lambda pt: pt[0])
         ch['points'] = np.array(ch['points'], dtype=np.float64)
         # timestamps = ch['points'][:,0]
@@ -177,62 +176,41 @@ def parse(bytes, parameters):
     for ch in channels.values():
         # calculate time axis delta / channel frequency
         delta = 100 # assume 100ms if a channel has a single point
-        if len(ch['points']) > 1:
+        if ch['n_points'] > 1:
             # find the most common time delta between points
             deltas = np.diff(ch['points'][:,0])
             unique_deltas, counts = np.unique(deltas, return_counts=True)
-            delta = min(int(unique_deltas[counts == counts.max()][0]), 100)
+            delta = min(int(unique_deltas[counts == counts.max()][0]), 100) # max delta = 100ms
             # round so that delta & frequency are integers
             while 1000 % delta != 0: delta += 1
-        # use this delta to get channel frequency (between 1Hz and 1000Hz)
+        # use this delta to get channel frequency
         ch['delta_ms'] = delta
-        ch['frequency_hz'] = math.trunc(max(1, min(1000, 1000 / ch['delta_ms'])))
+        ch['frequency_hz'] = math.trunc(1000 / ch['delta_ms'])
         ch['sample_count'] = math.trunc(ch['t_max'] / ch['delta_ms'])
-
-        # print()
         # create a new time axis with this frequency and sample count
-        t_int = list(range(ch['sample_count'])) * ch['delta_ms']
-        # get closest point to each time tick
-        v_int = [0] * ch['sample_count']
-        i = 0
-        j = 0
-        ts = 0
-        while j < ch['sample_count']:
-            if ch['points'][i][0] == ch['points'][0][0]:
-                if ts > ch['points'][i][0]:
-                    i += 1
-                else:
-                    v_int[j] = ch['points'][i][1]
-                    j += 1
-                    ts += ch['delta_ms']
-            else:
-                while ts > ch['points'][i+1][0]: i += 1
-                v_int[j] = ch['points'][i][1]
-                j += 1
-                ts += ch['delta_ms']
-
-        ch['t_int'] = np.array(t_int, dtype=np.float64)
-        ch['v_int'] = np.array(v_int, dtype=np.float64)
+        ch['t_int'] = np.arange(0, ch['delta_ms'] * ch['sample_count'], ch['delta_ms'])
+        # interpolate data over the new time axis
+        ch['v_int'] = np.interp(ch['t_int'], ch['points'][:,0], ch['points'][:,1])
     elapsed = round(time.time() - start, 2)
     print(f'({elapsed}s)')
 
     print('encoding channels... ', end='', flush=True)
     start = time.time()
-    for ch in channels.values():
-        ch['v_enc'] = []
-        ch['shift'], ch['scalar'], ch['divisor'], ch['scf'] = get_scalars(ch['id'], ch['v_min'], ch['v_max'])
-        if ch['divisor'] > 0:
+    for id in list(channels.keys()):
+        ch = channels[id]
+        try:
+            ch['shift'], ch['scalar'], ch['divisor'] = get_scalars(ch['v_min'], ch['v_max'])
+        except:
+            # encoding failed, remove channel
+            del channels[id]
+            print(f'failed to encode channel {id} min={ch['v_min']} max={ch['v_max']}')
+        else:
             ch['v_enc'] = np.array(
                 [v / 10**-ch['shift'] / ch['scalar'] * ch['divisor'] for v in ch['v_int']],
                 dtype=np.int32
             )
     elapsed = round(time.time() - start, 2)
     print(f'({elapsed}s)')
-
-    for id in list(channels.keys()):
-        # remove channels with failed encodings
-        if len(channels[id]['v_enc']) == 0:
-            del channels[id]
 
     print(f'created {len(channels)} channels')
     return channels
